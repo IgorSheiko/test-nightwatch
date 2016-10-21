28da4510e942b7991aa55e48034ca1290687dd83
Igor Sheiko <igor2109a@mail.ru>
2016-10-21 12:07:23 +0000

    test project

---
+++ nightwatch.conf.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "src_folders": [
+    "tests"// Where you are storing your Nightwatch e2e/UAT tests
+  ],
+  "output_folder": "./reports", // reports (test outcome) output by nightwatch
+  "selenium": { // downloaded by selenium-download module (see readme)
+    "start_process": true, // tells nightwatch to start/stop the selenium process
+    "server_path": "/home/vagrant/selenium/selenium-server-standalone-2.53.1.jar",
+    "host": "127.0.0.1",
+    "port": 4444, // standard selenium port
+    "cli_args": { // chromedriver is downloaded by selenium-download (see readme)
+      "webdriver.chrome.driver" : "/home/vagrant/selenium/chromedriver"
+    }
+  },
+  "test_settings": {
+    "default": {
+      "screenshots": {
+        "enabled": true, // if you want to keep screenshots
+        "path": './screenshots' // save screenshots here
+      },
+      "globals": {
+        "waitForConditionTimeout": 5000 // sometimes internet is slow so wait.
+      },
+      "desiredCapabilities": { // use Chrome as the default browser for tests
+        "browserName": "chrome"
+      }
+    },
+    "chrome": {
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true // set to false to test progressive enhancement
+      }
+    }
+  }
+}
--- tests/example.js
+++ tests/example.js
@@ -1,7 +1,10 @@
-module.exports = {
-  'it should load the page': function (browser) {
+module.exports = { // addapted from: https://git.io/vodU0
+  'Guinea Pig Assert Title': function(browser) {
     browser
-      .url('http://nightwatchjs.org')
+      .url('https://saucelabs.com/test/guinea-pig')
+      .waitForElementVisible('body')
+      .assert.title('I am a page title - Sauce Labs')
+      .saveScreenshot('ginea-pig-test.png')
       .end();
   }
 };
